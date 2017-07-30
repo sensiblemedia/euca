d49b0e547f4603f3b27243224592788836619512
root <git@sensible.co>
2017-07-30 15:25:43 +0100

    update

--- euca_stop.sh
+++ euca_stop.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-systemctl stop eucalyptus-cloud.service
+#systemctl stop eucalyptus-cloud.service
 systemctl stop eucalyptus-cluster.service
 systemctl stop eucanetd.service
 systemctl stop tgtd.service
